c2794c308215ec4d102f493a5c46f1aa9383acd4
t4top <temitope@yahoo.co.jp>
2023-12-16 23:59:43 +0100

    Add a sample code to show wallet connect using useWallet hook

--- packages/starter/react/create-react-app-example/src/App.tsx
+++ packages/starter/react/create-react-app-example/src/App.tsx
@@ -1,26 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { FC, ReactNode, useCallback } from "react";
+import type { WalletError, WalletName } from "mina-wallet-adapter-core";
+import { type AdapterOption, AdapterId, WalletProvider, useWallet } from "mina-wallet-adapter-ui-react";
 
-function App() {
+import "./App.css";
+
+const App: FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context>
+      <Content />
+    </Context>
   );
-}
+};
 
 export default App;
+
+const Context: FC<{ children: ReactNode }> = ({ children }) => {
+  const adapters: AdapterOption[] = [AdapterId.AURO];
+
+  function onError(error: WalletError) {
+    console.error(error);
+  }
+
+  return (
+    <WalletProvider adapters={adapters} autoConnect onError={onError}>
+      {children}
+    </WalletProvider>
+  );
+};
+
+const Content: FC = () => {
+  const AuroWalletName = "Auro" as WalletName<"Auro">;
+  const { connected, select } = useWallet();
+
+  const handleClick = useCallback(
+    (event: React.MouseEvent<HTMLButtonElement, MouseEvent>, walletName: WalletName) => {
+      if (select) select(walletName);
+    },
+    [select]
+  );
+
+  return (
+    <div className="App">
+      <h1>Square - Demo zkApp</h1>
+      <p>
+        Demo of{" "}
+        <a href="https://github.com/aztemi/mina-wallet-adapter" target="_blank" rel="noreferrer">
+          mina-wallet-adapter for implementing zkApps in Svelte.
+        </a>
+      </p>
+      <div>
+        <button onClick={event => handleClick(event, AuroWalletName)} disabled={connected}>
+          Connect
+        </button>
+      </div>
+    </div>
+  );
+};
