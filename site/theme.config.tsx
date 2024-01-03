10ad121e6044ff56b956ad1e9ef44c63ed3f601f
t4top <temitope@yahoo.co.jp>
2024-01-03 18:30:24 +0100

    Rename 'docs' to 'site' which will be the project website

--- site/theme.config.tsx
+++ site/theme.config.tsx
@@ -6,7 +6,7 @@ const config: DocsThemeConfig = {
   project: {
     link: "https://github.com/aztemi/mina-wallet-adapter"
   },
-  docsRepositoryBase: "https://github.com/aztemi/mina-wallet-adapter",
+  docsRepositoryBase: "https://github.com/aztemi/mina-wallet-adapter/tree/main/site",
   editLink: {
     text: "Edit this page on GitHub →"
   },
