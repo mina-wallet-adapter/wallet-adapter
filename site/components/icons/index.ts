ee741c51f246cdf8bed7be7c65231458d6703d4f
t4top <temitope@yahoo.co.jp>
2024-01-04 23:13:55 +0100

    Update docs and icons for sub-categories

---
+++ site/components/icons/index.ts
@@ -0,0 +1,7 @@
+export { default as AuroIcon } from "./aurowallet.svg";
+export { default as GlobeIcon } from "./globe.svg";
+export { default as LightningIcon } from "./lightning.svg";
+export { default as ReactIcon } from "./react.svg";
+export { default as SvelteIcon } from "./svelte.svg";
+export { default as TerminalIcon } from "./terminal.svg";
+export { default as WalletIcon } from "./wallet.svg";
