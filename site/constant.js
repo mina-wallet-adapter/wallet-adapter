ba238ee131569f87d3145acdf2e56794aa88767c
t4top <temitope@yahoo.co.jp>
2024-01-17 14:24:59 +0100

    Add SEO info and Open Graph details

--- site/constant.js
+++ site/constant.js
@@ -1,6 +1,6 @@
 export const isGithub = process.env.BUILD_ENV === "github";
 
-// Set Github repo name as the sub-path prefix when this site is intended for Github Pages
+// Set GitHub repo name as the sub-path prefix when site is built for GitHub Pages
 export const assetPrefix = isGithub ? "/mina-wallet-adapter" : "";
 
 export const addAssetPrefix = path => `${assetPrefix}${path}`;
--- site/theme.config.tsx
+++ site/theme.config.tsx
@@ -1,10 +1,54 @@
 import React from "react";
+import { useRouter } from "next/router";
+import { useConfig } from "nextra-theme-docs";
 import type { DocsThemeConfig } from "nextra-theme-docs";
 
+const site = {
+  name: "Mina Wallet Adapter",
+  url: "https://aztemi.github.io/mina-wallet-adapter",
+  description: "Add wallets support to your zkApps with ease."
+};
+
 const config: DocsThemeConfig = {
-  logo: <span>Mina Wallet Adapter</span>,
+  logo: <span>{site.name}</span>,
   nextThemes: {
     defaultTheme: "dark"
+  },
+  useNextSeoProps() {
+    const { asPath } = useRouter();
+    if (asPath !== "/") {
+      return {
+        titleTemplate: `%s – ${site.name}`
+      };
+    }
+  },
+  head: function useHead() {
+    const { title } = useConfig();
+    const pageTitle = (title ? title + " – " : "") + site.name;
+    const socialCard = `${site.url}/og.png`;
+
+    return (
+      <>
+        <meta httpEquiv="Content-Language" content="en" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="apple-mobile-web-app-title" content={site.name} />
+        <meta name="description" content={site.description} />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta property="twitter:url" content={site.url} />
+        <meta property="twitter:title" content={pageTitle} />
+        <meta property="twitter:image" content={socialCard} />
+        <meta property="twitter:description" content={site.description} />
+
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en" />
+        <meta property="og:url" content={site.url} />
+        <meta property="og:title" content={pageTitle} />
+        <meta property="og:image" content={socialCard} />
+        <meta property="og:description" content={site.description} />
+        <meta property="og:site_name" content={site.name} />
+      </>
+    );
   },
   project: {
     link: "https://github.com/aztemi/mina-wallet-adapter"
