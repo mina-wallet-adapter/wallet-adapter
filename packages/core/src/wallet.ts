58ea195e213fd52853eeb9904223349c13f7a0ca
t4top <temitope@yahoo.co.jp>
2024-04-26 12:46:53 +0200

    Simplify wallet events registration logic by reusing Wallet Standard API

--- packages/core/src/wallet.ts
+++ packages/core/src/wallet.ts
@@ -1,9 +1,4 @@
-import type {
-  Wallet,
-  WalletEventsWindow,
-  WindowRegisterWalletEvent,
-  WindowRegisterWalletEventCallback
-} from "@wallet-standard/base";
+export { registerWallet } from "@wallet-standard/core";
 
 /** WalletName is a nominal type. Wallet adapters should use like: `'MyMinaWallet' as WalletName<'MyMinaWallet'>` */
 export type WalletName<T extends string = string> = T & { __brand__: "WalletName" };
@@ -30,53 +25,4 @@ export enum WalletReadyState {
    * mobile) then it will stay in the `Unsupported` state.
    */
   Unsupported = "Unsupported"
-}
-
-export function registerWallet(wallet: Wallet): void {
-  const callback: WindowRegisterWalletEventCallback = ({ register }) => register(wallet);
-
-  try {
-    (window as WalletEventsWindow).dispatchEvent(new RegisterWalletEvent(callback));
-  } catch (error) {
-    console.error("wallet-standard:register-wallet event could not be dispatched", error);
-  }
-
-  try {
-    (window as WalletEventsWindow).addEventListener("wallet-standard:app-ready", ({ detail: api }) => callback(api));
-  } catch (error) {
-    console.error("wallet-standard:app-ready event listener could not be added", error);
-  }
-}
-
-class RegisterWalletEvent extends Event implements WindowRegisterWalletEvent {
-  readonly _detail: WindowRegisterWalletEventCallback;
-
-  constructor(callback: WindowRegisterWalletEventCallback) {
-    super("wallet-standard:register-wallet", {
-      bubbles: false,
-      cancelable: false,
-      composed: false
-    });
-    this._detail = callback;
-  }
-
-  get detail() {
-    return this._detail;
-  }
-
-  get type() {
-    return "wallet-standard:register-wallet" as const;
-  }
-
-  preventDefault(): never {
-    throw new Error("preventDefault cannot be called");
-  }
-
-  stopImmediatePropagation(): never {
-    throw new Error("stopImmediatePropagation cannot be called");
-  }
-
-  stopPropagation(): never {
-    throw new Error("stopPropagation cannot be called");
-  }
 }
